f96dc873b108e78a9e9312520e821979d2c5e1b2
bavellifrancesco-art <bavellifrancesco@gmail.com>
2026-07-14 21:38:50 +0200

    Create RenderDistanceMacro.java

---
+++ src/main/java/com/example/renderdistancemacro/RenderDistanceMacro.java
@@ -0,0 +1,10 @@
+package com.example.renderdistancemacro;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RenderDistanceMacro implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Client-only mod, no common logic needed.
+    }
+}
